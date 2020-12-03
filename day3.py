003757c4327aee88aa3b08f0fc51cc5de24552fa
WilliamCarlos <masteratsoccer@gmail.com>
2020-12-02 22:10:26 -0800

    toboggo

--- day3.py
+++ day3.py
@@ -7,7 +7,7 @@ with open("data/day3.txt") as f:
         grid.append(line)
 
 num_rows, num_cols = len(grid), len(grid[0])
-print('{} {}\n'.format(num_rows, num_cols))
+print('rows, cols: {} {}'.format(num_rows, num_cols))
 
 # Slope as (drow, dcol)
 slopes = [(1, 1), (1, 3), (1, 5), (1, 7), (2, 1)]
@@ -34,7 +34,14 @@ for dr, dc in slopes:
     num_trees_list.append(num_trees)
 
     # Answer for part 1.
-    if ((dr, dc) == (1, 3)): print(num_trees)
+    if ((dr, dc) == (1, 3)): print('\nPart 1: {}'.format(num_trees))
+
+slopes_and_trees = sorted(zip(slopes, num_trees_list), key=lambda x: x[1])
+print('You should probably take slope {}... {} trees :o\n'.format(*slopes_and_trees[0]))
+
+for slope, trees in list(slopes_and_trees):
+    print('slope {}: {} trees'.format(slope, trees))
 
 # Answer for part 2.
+print('\nPart2:')
 print(reduce(lambda x, y: x*y, num_trees_list))
